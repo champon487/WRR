3a5b9d8c355806aa1f4a9b75415700a76c4fd687
toshiki <toshia413@gmail.com>
2021-01-29 09:17:51 +0900

    河床変動モデル

---
+++ 1DBedEvolutionModel/SWE-Bed-Exner.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 16:13:50 2019
+
+@author: river801
+"""
+
+from numba import njit, jit, f8
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+import time
+
+@jit
+def momentum2(h,eta,u,wu,q,g,rdx,dt,snm,nx):
+    
+    for i in np.arange(1,nx):
+        h_bar = (h[i]+h[i+1])*0.5
+        pressure = -g*(-h[i]+h[i+1])*rdx-g*(-eta[i]+eta[i+1])*rdx
+        roughness = g*snm**2.0*u[i]/h_bar**(4.0/3.0)
+        advection = u[i]*(-u[i-1]+u[i])*rdx
+        wu[i] = (u[i]+(-advection+pressure)*dt)/(1.0+roughness*dt)
+        q[i] = wu[i]*h_bar
+        
+        wu[nx] = wu[nx-1]
+        q[nx] = q[nx-1]
+    
+    return wu, q
+
+@jit
+def continuity2(h,q,rdx,dt,nx):
+    
+    for i in np.arange(1,nx):
+        h[i] = h[i]-(-q[i-1]+q[i])*rdx*dt
+        
+    h[nx] = h[nx-1]
+    
+    return h
+
+@jit
+def bedload(h,u,qb,qbin,snm,spec,diam,tsc,g,nx):
+        
+    for i in np.arange(1,nx):
+        h_bar = (h[i]+h[i+1])*0.5
+        ts = (snm*u[i])**2./(spec*diam*h_bar**(1./3.))
+        if ts>tsc:
+            qb[i] = 8.0*(ts-tsc)**1.5*(spec*g*diam**3.)**0.5
+        else:
+            qb[i] = 0.
+            
+    qb[ 0] = qbin
+    qb[nx] = qb[nx-1]
+         
+    return qb
+
+@jit
+def exner(eta,qb,rdx,dt,poro,nx):
+        
+    for i in np.arange(1,nx+1):
+        eta[i] = eta[i]-(-qb[i-1]+qb[i])/dx*dt/(1.-poro)
+        
+    return eta
+
+# set workiing directly and file name
+
+
+# Setting the river geometry and model parameters
+
+g = 9.81
+nu = 0.000001
+
+q_min  = 100.          # minimun discharge m3/s
+q_max  = 600.         # maximum discharge m3/s
+t_q    = 20.*3600.   # time from min discharge to maximum discharge
+chlen  = 6000       # length of river reach (m)
+wid    = 70.            # channel width (m)
+snm    = 0.028           # mannings roughness coefs
+ib     = 0.005      # bed slope
+spec   = 1.65
+diam   = 0.04   # sediment diameter
+tsc    = 0.05
+poro   = 0.4
+qbin   = 0.01   #sediment supply m2/s
+
+i_vis = 0
+
+tuk   = 3600.*4
+etime = t_q*2.*50.
+
+nx = 200    # number of grid
+dx = chlen/float(nx)    # size of grid
+dt = 0.5    # computational time step (sec)
+
+rdx = 1./dx
+
+x   = np.zeros(nx+1)
+z   = np.zeros(nx+1)
+h   = np.zeros(nx+1)
+u   = np.zeros(nx+1)
+wu  = np.zeros(nx+1)
+q   = np.zeros(nx+1)
+hh  = np.zeros(nx+1)
+xc  = np.zeros(nx+1)
+eta = np.zeros(nx+1)
+qb  = np.zeros(nx+1)
+
+dzdx = np.zeros(nx+1)
+
+x[0] = 0.
+
+for i in np.arange(1,nx+1):
+    x[i] = x[i-1]+dx
+    
+
+z00  = chlen*ib
+z[0] = z00
+
+for i in np.arange(1,nx+1):
+    z[i] = z[i-1]-ib*dx
+        
+for i in np.arange(1,nx+1):
+    eta[i] = (z[i-1]+z[i])*0.5
+    xc[i] = (x[i-1]+x[i])*0.5
+    
+h0 = (snm*q_min/(wid*ib**0.5))**0.6
+
+hmax = (snm*q_max/(wid*ib**0.5))**0.6
+
+h[:] = h0
+u[:] = q_min/(h0*wid)
+q[:] = q_min/wid
+
+tt = 0.
+t_hyd = 0.
+optime = 0.
+
+n = 0
+
+t0 = time.time()
+    
+while tt<etime:
+    
+    if t_hyd<t_q:
+        qt = q_min+(q_max-q_min)/t_q*t_hyd
+    else:
+        qt = q_max-(q_max-q_min)/t_q*(t_hyd-t_q)
+    
+    h[0] = (snm*qt/(wid*ib**0.5))**0.6
+    u[0] = qt/(h[0]*wid)
+    q[0] = qt/wid
+    
+    momentum2(h,eta,u,wu,q,g,rdx,dt,snm,nx)
+    
+    continuity2(h,q,rdx,dt,nx)
+    
+    u = wu
+    
+    bedload(h,u,qb,qbin,snm,spec,diam,tsc,g,nx)
+        
+    exner(eta,qb,rdx,dt,poro,nx)
+        
+    if optime>tuk:
+        
+        print("Time= ",tt/3600)
+        optime = optime-tuk
+        
+        hh = h+eta
+        
+        plt.xlim([0,chlen])
+        plt.xlabel( "Downstream distance (m)" ) 
+        
+        if i_vis==0:
+        
+            plt.ylim([0,z00+hmax*10.])
+            
+            plt.plot(xc[1:nx],eta[1:nx],color='k',label='Bed surface')
+            plt.plot(xc[1:nx],hh[1:nx],color='b',label='Water surface')
+            plt.ylabel( "Elevation (m)" )
+        
+        else:
+        
+            plt.ylim([0.5,1.5])
+            
+            dzdx[1:nx] = -(-eta[1:nx]+eta[2:nx+1])*rdx/ib
+            
+            plt.plot(xc[1:nx],dzdx[1:nx],color='b',label='slope')
+            
+            plt.ylabel( "slope/initial slope" )
+        
+        plt.legend()
+        
+        nnn = str(n)
+        
+        plt.savefig('fig/Figure' + nnn.zfill(4) +".jpg", dpi=300)
+        
+        plt.close()
+        
+        n += 1
+    
+    optime+=dt
+    tt+=dt
+    t_hyd+=dt
+    
+    if t_hyd>2.*t_q:
+        t_hyd = t_hyd-2.*t_q
+    
+t1 = time.time()
+print("Computational time= ",t1-t0)
+
+# nx=100 t=100
+# jit 19 sec
+# normal 270 sec
+# normal 319 sec
+
+# nx=100 t=300
+# jit 53 sec
+# normal 806 sec
+
